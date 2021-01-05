0584a116892b39935a3306bf2320c2f66a71bb07
Lucas Zubeldia <39446606+dr-van-nostrand@users.noreply.github.com>
2021-01-05 18:54:51 -0300

    Add files via upload

---
+++ build/precache-manifest.2c684e3c436f836cd1aff2cc2d0a83a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7899e53bfa7a6762617c",
+    "url": "/static/css/main.d71370b3.chunk.css"
+  },
+  {
+    "revision": "7899e53bfa7a6762617c",
+    "url": "/static/js/main.7899e53b.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "21f234fda44fbb3ee94715419aa06506",
+    "url": "/index.html"
+  }
+];
