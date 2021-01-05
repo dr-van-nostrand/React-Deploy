bb86d8e19204950a30d27d869ed2943c7c42f6ce
Lucas Zubeldia <lzubeldia@gmail.com>
2021-01-05 19:01:36 -0300

    Updates

---
+++ precache-manifest.54be022ed7a5b201527f732f8921a993.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e36109e89e00ca2c0495",
+    "url": "/React-Deploy/static/css/main.87b21db9.chunk.css"
+  },
+  {
+    "revision": "e36109e89e00ca2c0495",
+    "url": "/React-Deploy/static/js/main.e36109e8.chunk.js"
+  },
+  {
+    "revision": "25a97381f2fd20d18f35",
+    "url": "/React-Deploy/static/js/1.25a97381.chunk.js"
+  },
+  {
+    "revision": "c76b447826802e115136",
+    "url": "/React-Deploy/static/js/runtime~main.c76b4478.js"
+  },
+  {
+    "revision": "9d702f86f3b8689f5d418424ba1cdfb8",
+    "url": "/React-Deploy/index.html"
+  }
+];
